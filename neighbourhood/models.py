e9912769d9c92cfa95025796f898fb0cc2a28a08
Paulwamaria <paulwamaria@gmail.com>
2019-10-26 09:25:35 +0300

    Add class Neighborhood

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Neighborhood(models.Model):
+    neighborhood_name = models.CharField(max_length=60)
+    neighborhood_location = models.CharField(max_length=60)
+    image = models.ImageField(upload_to='media/hood/', blank = True, null =True)
+    occupants = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.neighborhood_name
+
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
