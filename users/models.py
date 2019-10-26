4167aa08cd7122ee78b8db143319ab6813ef1034
Paulwamaria <paulwamaria@gmail.com>
2019-10-26 13:01:50 +0300

    Add location field

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic =  models.ImageField(upload_to='media/profile_pics', default='media/default.jpg')
     bio = models.TextField()
+    location= models.CharField(max_length = 60, blank = True)
    
 
 
