43b1636a38dad11a3aa392972157d174a43dccab
Paulwamaria <paulwamaria@gmail.com>
2019-10-26 13:00:21 +0300

    Add user and profile update forms

--- users/forms.py
+++ users/forms.py
@@ -44,3 +44,22 @@ class NeighborhoodRegistrationForm(UserCreationForm):
 
 
         return user
+
+
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required = True,label='',widget=forms.TextInput(attrs = {'class':'form-control','placeholder':'Email'}))
+    first_name = forms.CharField(max_length=256,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
+    last_name = forms.CharField(max_length=256,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
+    username = forms.CharField(max_length=60,required = True, label='', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Username'}))
+
+    class Meta:
+        model = User
+        fields = ['email','first_name', 'last_name','username']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_pic','bio']
