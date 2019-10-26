b60af728941264558eaa7ba26c57490f0bc9c3c7
Paulwamaria <paulwamaria@gmail.com>
2019-10-26 13:33:10 +0300

    Add a funtion to update profile

--- users/views.py
+++ users/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-from .forms import NeighborhoodRegistrationForm
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import NeighborhoodRegistrationForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Profile
+
+
 
 def register(request):
     if request.method == 'POST':
@@ -19,3 +26,24 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance = request.user)
+        
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance = request.user.profile)    
+        if u_form.is_valid and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+    else:
+        u_form = UserUpdateForm(instance = request.user)
+        
+        p_form = ProfileUpdateForm(instance = request.user.profile)
+
+
+    context={
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request, 'users/profile.html',context)
