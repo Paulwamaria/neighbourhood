db3aac282e7694d5aa17a36da65983a8b805493a
Paulwamaria <paulwamaria@gmail.com>
2019-10-26 09:45:52 +0300

    Add class Post and add related_names to other classes

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -22,3 +22,50 @@ class Neighborhood(models.Model):
     @classmethod
     def delete_neighbourhood(cls,neighbourhood):
         cls.objects.filter(neighbourhood=neighbourhood).delete()
+
+
+
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=60)
+    description = models.TextField()
+    neighborhood = models.ForeignKey(Neighborhood, related_name='businesses',  on_delete=models.CASCADE)
+    logo = models.ImageField(upload_to='media/business/', blank = True, null =True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    business_email = models.EmailField()
+
+
+    def __str__(self):
+        return self.business_name
+
+
+    def save_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    content = models.TextField()
+    neighborhood = models.ForeignKey(Neighborhood, related_name='posts', on_delete=models.CASCADE)
+    logo = models.ImageField(upload_to='media/blog/', blank = True, null =True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    business_email = models.EmailField()
+
+
+    def __str__(self):
+        return self.title
+
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
