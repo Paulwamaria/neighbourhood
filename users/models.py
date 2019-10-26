16601aa6a9cf6befd85adb61770265ad755d06dd
Paulwamaria <paulwamaria@gmail.com>
2019-10-26 11:21:22 +0300

    Ccreate class profile

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic =  models.ImageField(upload_to='media/profile_pics', default='media/default.jpg')
+    bio = models.TextField()
+   
+
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
