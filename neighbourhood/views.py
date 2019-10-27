80965d7c0d434b76cce38aec2ae8f46a7d3821ef
Paulwamaria <paulwamaria@gmail.com>
2019-10-27 11:07:48 +0300

    Add a funtion to display posts

--- neighbourhood/views.py
+++ neighbourhood/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import Neighborhood,Post
+from users.models import Profile
 
 def index(request):
-    message = "Hello World"
+    message = " Whats Going on in your Neighbourhood?"
+    neighborhoods = Neighborhood.objects.all()
+
+
     context = {
-        "message":message
+        "message":message,
+        "neighs":neighborhoods,
+      
+    
     }
 
     return render(request,'neighbourhood/index.html',context)
 
+
+@login_required
+def post(request):
+  
+    profile=Profile.objects.get(user=request.user)
+    posts = Post.objects.filter(neighborhood__neighborhood_name=profile.location)
+
+    
+
+    context={
+        "posts":posts
+    }
+
+    return render(request,'neighbourhood/posts.html',context)
