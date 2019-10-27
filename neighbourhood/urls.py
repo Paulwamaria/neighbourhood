5eb32d74929a1f1a5979d3d70927e91c83b7282f
Paulwamaria <paulwamaria@gmail.com>
2019-10-27 12:37:58 +0300

    Add a url for creating posts

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from .views import PostCreateView
 from . import views
 
 
 urlpatterns=[
     path('',views.index,name='home'),
-    path('posts/',views.post,name='posts')
+    path('posts/',views.post,name='posts'),
+    path('post/new/',PostCreateView.as_view(), name = 'post-create')
 ]
