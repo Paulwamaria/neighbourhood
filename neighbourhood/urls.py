cc7d57d932ff694ad40dac45e844e248233b1f96
Paulwamaria <paulwamaria@gmail.com>
2019-10-27 18:46:44 +0300

    Add a url for profile details

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -9,4 +9,5 @@ urlpatterns=[
     path('business/',views.business_list,name='business'),
     path('post/new/',PostCreateView.as_view(), name = 'post-create'),
     path('business/new/',BusinessCreateView.as_view(), name = 'business-create'),
+    path('profile/details/<str:username>/',views.display_profile, name = 'profile-detail'),
 ]
