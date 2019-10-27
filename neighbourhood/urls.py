0f79882671626b8ddcbc32e59cc84d3e44cf94a5
Paulwamaria <paulwamaria@gmail.com>
2019-10-27 19:11:05 +0300

    Add urls for updating and deleting posts

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostCreateView,BusinessCreateView
+from .views import PostCreateView,BusinessCreateView,PostUpdateView,PostDeleteView
 from . import views
 
 
@@ -10,4 +10,6 @@ urlpatterns=[
     path('post/new/',PostCreateView.as_view(), name = 'post-create'),
     path('business/new/',BusinessCreateView.as_view(), name = 'business-create'),
     path('profile/details/<str:username>/',views.display_profile, name = 'profile-detail'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(), name = 'post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(), name = 'post-delete'),
 ]
