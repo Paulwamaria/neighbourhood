6be4921177fa8d673e6061b5222b33a0629ba5e7
Paulwamaria <paulwamaria@gmail.com>
2019-10-27 13:42:35 +0300

    Add urls for businesses

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import PostCreateView
+from .views import PostCreateView,BusinessCreateView
 from . import views
 
 
 urlpatterns=[
     path('',views.index,name='home'),
     path('posts/',views.post,name='posts'),
-    path('post/new/',PostCreateView.as_view(), name = 'post-create')
+    path('business/',views.business_list,name='business'),
+    path('post/new/',PostCreateView.as_view(), name = 'post-create'),
+    path('business/new/',BusinessCreateView.as_view(), name = 'business-create'),
 ]
